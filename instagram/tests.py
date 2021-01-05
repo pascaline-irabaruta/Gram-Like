8a513f0aa97dd6cd15a917975c75dc7c31c87178
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-05 16:37:45 +0200

    created a test class for profile model

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Profile, Post
+from django.contrib.auth.models import User
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='charles')
+        self.user.save()
+
+        self.profile_test = Profile(id=1, name='ifoto', profile_picture='defaulty.jpg', bio='profile test',
+                                    user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
