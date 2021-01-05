b9c22e6b8485d3381a8120a4504fd5252caf7d93
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-05 16:10:00 +0200

    created a method that returns the total lokes on a post

--- instagram/models.py
+++ instagram/models.py
@@ -56,3 +56,9 @@ class Post(models.Model):
 
     def delete_image(self):
         self.delete()
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.user.name} Post'
