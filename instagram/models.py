35429d5dc49beee0f7203cf7a48615a003bb2088
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-05 16:06:08 +0200

    created the post model

--- instagram/models.py
+++ instagram/models.py
@@ -32,3 +32,14 @@ class Profile(models.Model):
     @classmethod
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    name = models.CharField(max_length=250, blank=True)
+    caption = models.CharField(max_length=250, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
